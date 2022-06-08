ac706828fc0364c33e8fd652f15f8f910c55bc40
Avery Avila <averyavila@fb.com>
2022-06-08 16:22:17 -0700

    Full build  + User can open the twitter app offline and see last loaded tweets persisted into SQLite

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetDao.java
@@ -0,0 +1,24 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import static android.icu.text.MessagePattern.ArgType.SELECT;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface TweetDao {
+
+    @Query("SELECT Tweet.body as tweet_body, Tweet.createdAt as tweet_createdAt, Tweet.id as tweet_id, Tweet.mediaHTTPS as tweet_mediaHTTPS, User.*" +
+            " FROM Tweet INNER JOIN User ON Tweet.userId = User.id ORDER BY Tweet.createdAt DESC LIMIT 25")
+    List<TweetWithUser> recentItems();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertModel(Tweet... tweets);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertModel(User... users);
+}
